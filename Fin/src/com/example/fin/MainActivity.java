01a69c22a386120fb02ca7987b9af06d084e4140
karthikccet <karthik.venkatachalam1@gmail.com>
2013-01-18 19:54:58 +0530

    7_54

--- Fin/src/com/example/fin/MainActivity.java
+++ Fin/src/com/example/fin/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.fin;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.view.Menu;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,9 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		IotdHandler ih=new IotdHandler();
+		ih.processFeed();
+		resetDisplay(ih.getTitle(),ih.getDate(),ih.getImage(), ih.getDescription());
 	}
 
 	@Override
@@ -18,5 +24,17 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
+	public void resetDisplay(String title, String date,	Bitmap imageUrl, StringBuffer stringBuffer){
+		TextView tw1=(TextView)findViewById(R.id.textView1);
+		tw1.setText(title);
+		TextView tw2=(TextView)findViewById(R.id.textView2);
+		tw1.setText(date);
+		ImageView iv=(ImageView)findViewById(R.id.imageView1);
+		iv.setImageBitmap(imageUrl);
+		TextView tw3=(TextView)findViewById(R.id.textView3);
+		tw1.setText(stringBuffer);
+		
+	}
+	
 
 }
